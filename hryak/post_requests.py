7a4da45e221a328ca5b66f7f182d5df75c6d7cb1
brevnoo <boikooleksandr25@gmail.com>
2025-04-15 12:36:39 -0400

    Initial commit

---
+++ hryak/post_requests.py
@@ -0,0 +1,3 @@
+def feed(user_id):
+    weight_gain = 3  # real logic goes here
+    return {"user_id": user_id, "weight": weight_gain}
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='hryak',
+    version='0.1.0',
+    packages=find_packages(),
+    description='Logic package for Hryak',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+    ],
+    python_requires='3.12',
+)
