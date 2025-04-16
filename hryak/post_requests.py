39ecde515f76445dd8e4deddccc4a21802c3312b
brevnoo <boikooleksandr25@gmail.com>
2025-04-16 02:03:59 -0400

    Some Init Changes

--- hryak/post_requests.py
+++ hryak/post_requests.py
@@ -8,7 +8,7 @@ from . import config
 def feed(user_id: int, client = None):
     ready_to_feed = Pig.is_ready_to_feed(user_id)
     if not ready_to_feed:
-        return {'status': '400;not_ready'}
+        return {'status': '400;not_ready', 'try_again': Pig.get_time_of_next_feed(user_id)}
     Stats.add_pig_fed(user_id, 1)
     buffs_to_give = GameFunc.calculate_buff_multipliers(user_id, use_buffs=True, client=client)
 
@@ -38,3 +38,16 @@ def feed(user_id: int, client = None):
         Stats.add_streak(user_id)
         History.add_streak_to_history(user_id, Func.generate_current_timestamp(), 'feed')
     return {"status": 'success', "weight_added": weight_add, "pooped_amount": pooped_amount, "vomit": vomit}
+
+def butcher(user_id: int):
+    ready_to_butcher = Pig.is_ready_to_butcher(user_id)
+    if not ready_to_butcher:
+        return {'status': '400;not_ready', 'try_again': Pig.get_time_of_next_butcher(user_id)}
+    if Item.get_amount('knife', user_id) <= 0:
+        return {'status': '400;no_item;knife'}
+    lard_add = random.randrange(8, 16)
+    User.add_item(user_id, 'lard', lard_add)
+    weight_lost = round(random.uniform(.2, .7) * lard_add, 1)
+    Pig.add_weight(user_id, -weight_lost)
+    History.add_butcher_to_history(user_id, Func.generate_current_timestamp())
+    return {"status": 'success', "lard_added": lard_add, "weight_lost": weight_lost}
