4a4ce708980be7bd271b3b2f9d51786ca0448a0d
brevnoo <boikooleksandr25@gmail.com>
2025-04-16 01:49:29 -0400

    Some Init Changes

--- hryak/setters.py
+++ hryak/setters.py
@@ -1,23 +1,17 @@
-from .config import (logs_path, test, pig_feed_cooldown,
-                     pig_butcher_cooldown, streak_timeout)
+from . import config
 
 
 def set_logs_path(path: str):
-    global logs_path
-    logs_path = path
+    config.logs_path = path
 
 def set_test_mode(mode: bool):
-    global test
-    test = mode
+    config.test = mode
 
 def set_pig_feed_cooldown(cooldown: int):
-    global pig_feed_cooldown
-    pig_feed_cooldown = cooldown
+    config.pig_feed_cooldown = cooldown
 
 def set_pig_butcher_cooldown(cooldown: int):
-    global pig_butcher_cooldown
-    pig_butcher_cooldown = cooldown
+    config.pig_butcher_cooldown = cooldown
 
 def set_streak_timeout(timeout: int):
-    global streak_timeout
-    streak_timeout = timeout
+    config.streak_timeout = timeout
