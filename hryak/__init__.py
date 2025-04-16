96209b74ae2a1a1ac85cba27aed54a59c11d5efc
brevnoo <boikooleksandr25@gmail.com>
2025-04-16 09:46:45 -0400

    Some Init Changes

--- hryak/__init__.py
+++ hryak/__init__.py
@@ -1,6 +1,6 @@
 from . import requests
 from .functions import Func
-from . import game_functions
+from .game_functions import GameFunc
 from . import config
 from . import db_api
 from . import locale
