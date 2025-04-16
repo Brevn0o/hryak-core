6044a1918aaac758fd233d9ace6fca6c816391e2
brevnoo <boikooleksandr25@gmail.com>
2025-04-16 01:10:40 -0400

    Some Init Changes

--- hryak/setters.py
+++ hryak/setters.py
@@ -1,5 +1,10 @@
 logs_path = None
+test = False
 
-def set_logs_path(path):
+def set_logs_path(path: str):
     global logs_path
     logs_path = path
+
+def set_test_mode(mode: bool):
+    global test
+    test = mode
