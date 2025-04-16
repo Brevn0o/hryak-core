0827f20f3f37b4766b3ff6830a09502988feb551
brevnoo <boikooleksandr25@gmail.com>
2025-04-16 02:44:16 -0400

    Some Init Changes

--- hryak/db_api/__init__.py
+++ hryak/db_api/__init__.py
@@ -5,6 +5,7 @@ from .pig import Pig
 from .item import Item
 from .tech import Tech
 from .user import User
+from .trade import Trade
 from .history import History
 from .setup import Setup
 from .guild import Guild
