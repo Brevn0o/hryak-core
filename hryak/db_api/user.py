4451edbda2b62001e6d4a4a7e1dc9d8e48446900
brevnoo <boikooleksandr25@gmail.com>
2025-04-16 12:49:40 -0400

    Some Init Changes

--- hryak/db_api/user.py
+++ hryak/db_api/user.py
@@ -93,23 +93,26 @@ class User:
 
     @staticmethod
     def add_item(user_id, item_id, amount: int = 1, log: bool = True):
-        query = """
-        UPDATE users
-        SET inventory = JSON_SET(
-            inventory,
-            CONCAT('$.\"', %s, '\".amount'),
-            CAST(
-                COALESCE(
-                    CAST(JSON_UNQUOTE(JSON_EXTRACT(inventory, CONCAT('$.\"', %s, '\".amount'))) AS UNSIGNED),
-                    0
-                ) + %s
-                AS UNSIGNED
+        query = f"""
+            UPDATE users
+            SET inventory = JSON_SET(
+                inventory,
+                %s,
+                JSON_OBJECT(
+                    'amount',
+                    ROUND(COALESCE(
+                        JSON_EXTRACT(inventory, %s),
+                        0
+                    ) + %s)
+                )
             )
-        )
-        WHERE id = %s
-        """
+            WHERE id = %s;
+            """
 
-        Connection.make_request(query, (item_id, item_id, amount, user_id))
+        path_item = f"$.{item_id}"
+        path_amount = f"$.{item_id}.amount"
+
+        Connection.make_request(query, (path_item, path_amount, amount, user_id))
         if log:
             Func.add_log('item_generated',
                          user_id=user_id,
