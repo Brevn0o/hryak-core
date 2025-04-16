795c8ba0e4a0eb89be5b003500e785e3da0a737d
brevnoo <boikooleksandr25@gmail.com>
2025-04-16 01:19:47 -0400

    Some Init Changes

--- hryak/setters.py
+++ hryak/setters.py
@@ -1,5 +1,9 @@
 logs_path = None
 test = False
+pig_feed_cooldown = 4 * 3600
+pig_butcher_cooldown = 40 * 3600
+streak_timeout = 24.5 * 3600 if not test else 15
+
 
 def set_logs_path(path: str):
     global logs_path
@@ -8,3 +12,15 @@ def set_logs_path(path: str):
 def set_test_mode(mode: bool):
     global test
     test = mode
+
+def set_pig_feed_cooldown(cooldown: int):
+    global pig_feed_cooldown
+    pig_feed_cooldown = cooldown
+
+def set_pig_butcher_cooldown(cooldown: int):
+    global pig_butcher_cooldown
+    pig_butcher_cooldown = cooldown
+
+def set_streak_timeout(timeout: int):
+    global streak_timeout
+    streak_timeout = timeout
